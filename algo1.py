4680d44e1b4263cc25fa695b004b2d6ed6bbd7ee
brendanwong-web <69905640+brendanwong-web@users.noreply.github.com>
2023-03-17 16:52:24 +0800

    added code to limit trade vol to 20

--- algo1.py
+++ algo1.py
@@ -27,26 +27,31 @@ class Trader:
                 # Define a fair value for the PEARLS.
                 # Note that this value of 1 is just a dummy value, you should likely change it!
                 acceptable_price = 1
+                ask_vol_lim = 20
 
                 # If statement checks if there are any SELL orders in the PEARLS market
                 if len(order_depth.sell_orders) > 0:
+                    while ask_vol_lim > 0:
+                        # Sort all the available sell orders by their price,
+                        # and select only the sell order with the lowest price
+                        best_ask = min(order_depth.sell_orders.keys())
+                        best_ask_volume = order_depth.sell_orders[best_ask]
 
-                    # Sort all the available sell orders by their price,
-                    # and select only the sell order with the lowest price
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
+                        if best_ask_volume <= ask_vol_lim:
+                            # Check if the lowest ask (sell order) is lower than the above defined fair value, means we can buy
+                            if best_ask < acceptable_price:
 
-                    # Check if the lowest ask (sell order) is lower than the above defined fair value, means we can buy
-                    if best_ask < acceptable_price:
-
-                        # In case the lowest ask is lower than our fair value,
-                        # This presents an opportunity for us to buy cheaply
-                        # The code below therefore sends a BUY order at the price level of the ask,
-                        # with the same quantity
-                        # We expect this order to trade with the sell order
-                        # sign should be positive since it is a buy order
-                        print("BUY", str(-best_ask_volume) + "x", best_ask)
-                        orders.append(Order(product, best_ask, best_ask_volume))
+                                # In case the lowest ask is lower than our fair value,
+                                # This presents an opportunity for us to buy cheaply
+                                # The code below therefore sends a BUY order at the price level of the ask,
+                                # with the same quantity
+                                # We expect this order to trade with the sell order
+                                # sign should be positive since it is a buy order
+                                print("BUY", str(-best_ask_volume) + "x", best_ask)
+                                orders.append(Order(product, best_ask, best_ask_volume))
+                                ask_vol_lim -= best_ask_volume
+                        else:
+                            print('Current book ask vol is higher than quota')
 
                 # The below code block is similar to the one above,
                 # the difference is that it finds the highest bid (buy order)
@@ -67,54 +72,4 @@ class Trader:
                 # These possibly contain buy or sell orders for PEARLS
                 # Depending on the logic above
         
-            if product == 'BANANAS':
-
-                # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
-                order_depth: OrderDepth = state.order_depths[product]
-
-                # Initialize the list of Orders to be sent as an empty list
-                orders: list[Order] = []
-
-                # Define a fair value for the PEARLS.
-                # Note that this value of 1 is just a dummy value, you should likely change it!
-                acceptable_price = 1
-
-                # If statement checks if there are any SELL orders in the PEARLS market
-                if len(order_depth.sell_orders) > 0:
-
-                    # Sort all the available sell orders by their price,
-                    # and select only the sell order with the lowest price
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
-
-                    # Check if the lowest ask (sell order) is lower than the above defined fair value, means we can buy
-                    if best_ask < acceptable_price:
-
-                        # In case the lowest ask is lower than our fair value,
-                        # This presents an opportunity for us to buy cheaply
-                        # The code below therefore sends a BUY order at the price level of the ask,
-                        # with the same quantity
-                        # We expect this order to trade with the sell order
-                        # sign should be positive since it is a buy order
-                        print("BUY", str(-best_ask_volume) + "x", best_ask)
-                        orders.append(Order(product, best_ask, best_ask_volume))
-
-                # The below code block is similar to the one above,
-                # the difference is that it finds the highest bid (buy order)
-                # If the price of the order is higher than the fair value
-                # This is an opportunity to sell at a premium
-                # sign should be negative since it is a sell order
-                if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
-                        print("SELL", str(best_bid_volume) + "x", best_bid)
-                        orders.append(Order(product, best_bid, -best_bid_volume))
-
-                # Add all the above orders to the result dict
-                result[product] = orders
-
-                # Return the dict of orders
-                # These possibly contain buy or sell orders for PEARLS
-                # Depending on the logic above
-        return result
+            return result
